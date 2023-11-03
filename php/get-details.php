bdc62a651dfc7fe9df731fea0b5be219cedb5a53
cramr <93094049+kraemr@users.noreply.github.com>
2023-11-03 22:19:49 +0100

    added defs

--- php/get-details.php
+++ php/get-details.php
@@ -11,9 +11,20 @@ else{
 require "db-conn.php";
 $conn = connect_db();
 // Process other options here
-$query = $conn->prepare("Select * from accident_data where Id = ?");
+// LEFT JOIN here ??
+$query = 
+$conn->prepare(
+    "Select * from accident_data
+    INNER JOIN land_def ON land_def.land = accident_data.land 
+    INNER JOIN kind_def ON kind_def.kind = accident_data.kind 
+    INNER JOIN category_def ON category_def.category = accident_data.category
+    INNER JOIN type_def ON type_def.type = accident_data.type
+    where Id = ?
+    "
+);
 $query->bindValue(1,$id,PDO::PARAM_INT);
 $query->execute();  
-$data=$query->fetch();
+$data = $query->fetch(PDO::FETCH_ASSOC);
+
 echo json_encode($data); // im not gonna clean these values up :) its literally just one json_obj
 ?>
--- setup.sql
+++ setup.sql
@@ -1,3 +1,4 @@
+# https://stackoverflow.com/questions/4959610/sql-chaining-joins-efficiency
 CREATE DATABASE AccidentDB;
 use AccidentDB;
 Create Table accident_data(
@@ -22,16 +23,21 @@ Create Table accident_data(
     delivery_van_involved BOOLEAN,
     truck_bus_or_tram_involved BOOLEAN,
     road_surface_condition TINYINT,
-    coordinate_UTM_x DOUBLE, #LINREFX
-    coordinate_UTM_y DOUBLE, #LINREFY
-    longitude DOUBLE, # x
-    latitude DOUBLE, # y
+    coordinate_UTM_x FLOAT, #LINREFX 
+    coordinate_UTM_y FLOAT, #LINREFY
+    longitude FLOAT, # x
+    latitude FLOAT, # y
     CONSTRAINT PK_Accident_Data PRIMARY KEY (Id)
 );
 
 CREATE INDEX IX_accident_year ON accident_data(year);
 CREATE INDEX IX_accident_month ON accident_data(month);
 CREATE INDEX IX_accident_region ON accident_data(region);
+
+CREATE INDEX IX_accident_land ON accident_data(land);
+CREATE INDEX IX_accident_kind ON accident_data(kind);
+CREATE INDEX IX_accident_category ON accident_data(category);
+
 
 Create Table land_def(
     land SMALLINT,
@@ -45,21 +51,32 @@ Create Table munincipality_def(
     CONSTRAINT PK_munincipality_def PRIMARY KEY (munincipality)
 );
 
-Create Table kinds_of_accidents_def(
+Create Table kind_def(
     kind TINYINT, # Foreign Key 
     kind_str TINYTEXT,
     CONSTRAINT PK_kind_def PRIMARY KEY (kind)
 );
 
 
-Create Table category_of_accidents_def(
+Create Table category_def(
     category TINYINT, # Foreign Key 
     category_str TINYTEXT,
-    CONSTRAINT PK_kind_def PRIMARY KEY (kind)
+    CONSTRAINT PK_category_def PRIMARY KEY (category)
 );
+
+Create Table type_def(
+    type TINYINT, # Foreign Key 
+    type_str TINYTEXT,
+    CONSTRAINT PK_type_def PRIMARY KEY (type)
+);
+
 
 Create Table district_def(
     district MEDIUMINT, # Foreign Key
     district_str TINYTEXT,
     CONSTRAINT PK_district_def PRIMARY KEY (district)
 );
+
+CREATE INDEX IX_land_def ON land_def(land);
+CREATE INDEX IX_kind_def ON kind_def(kind);
+CREATE INDEX IX_cat_def  ON category_def(category);  
