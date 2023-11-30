da79df4e07e2049bc94397e6ffa82e6567bbf804
93094049+kraemr@users.noreply.github.com <93094049+kraemr@users.noreply.github.com>
2023-11-30 09:46:34 +0100

    added structure

--- php/db-conn.php
+++ php/db-conn.php
@@ -1,6 +1,7 @@
 <?php
 function connect_db(){
     $servername = "172.17.0.2";
+    #$servername = "localhost";
     $username = "root";
     $password = "Test";
     return new PDO("mysql:host=$servername;dbname=AccidentDB", $username, $password);
