3662868927e9994cac2b24aa336a1cc7017a8122
93094049+kraemr@users.noreply.github.com <93094049+kraemr@users.noreply.github.com>
2023-12-06 15:00:33 +0100

    added detail defs

--- php/util.php
+++ php/util.php
@@ -1,6 +1,6 @@
 <?php
 
-const JOINS=
+$JOINS=
 "INNER JOIN land_def ON land_def.land = accident_data.land INNER JOIN kind_def ON kind_def.kind = accident_data.kind" .
 " INNER JOIN category_def ON category_def.category = accident_data.category INNER JOIN type_def ON type_def.type = accident_data.type" .
 " INNER JOIN light_condition_def ON light_condition_def.light_condition = accident_data.light_condition" .
@@ -11,7 +11,6 @@ const JOINS=
 " INNER JOIN delivery_van_involved_def ON delivery_van_involved_def.delivery_van_involved = accident_data.delivery_van_involved" .
 " INNER JOIN truck_bus_or_tram_involved_def ON truck_bus_or_tram_involved_def.truck_bus_or_tram_involved = accident_data.truck_bus_or_tram_involved" .
 " INNER JOIN road_surface_condition_def ON road_surface_condition_def.road_surface_condition = accident_data.road_surface_condition";
-
 function send_json_err($msg){
     echo "{\"error\":\"$msg\"}";
 }
