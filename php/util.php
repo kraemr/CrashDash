9188848027ffab48d2aa34dad636815cf8b2758b
93094049+kraemr@users.noreply.github.com <93094049+kraemr@users.noreply.github.com>
2023-12-07 00:20:50 +0100

    detail view finished

--- php/util.php
+++ php/util.php
@@ -10,7 +10,8 @@ $JOINS=
 " INNER JOIN motorcycle_involved_def ON motorcycle_involved_def.motorcycle_involved = accident_data.motorcycle_involved" .
 " INNER JOIN delivery_van_involved_def ON delivery_van_involved_def.delivery_van_involved = accident_data.delivery_van_involved" .
 " INNER JOIN truck_bus_or_tram_involved_def ON truck_bus_or_tram_involved_def.truck_bus_or_tram_involved = accident_data.truck_bus_or_tram_involved" .
-" INNER JOIN road_surface_condition_def ON road_surface_condition_def.road_surface_condition = accident_data.road_surface_condition";
+" INNER JOIN road_surface_condition_def ON road_surface_condition_def.road_surface_condition = accident_data.road_surface_condition" .
+" INNER JOIN day_def ON day_def.day = accident_data.day";
 function send_json_err($msg){
     echo "{\"error\":\"$msg\"}";
 }
