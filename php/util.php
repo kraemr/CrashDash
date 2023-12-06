c178efc36f5deec4f4f36fc3af45e1b9a06e488e
93094049+kraemr@users.noreply.github.com <93094049+kraemr@users.noreply.github.com>
2023-12-06 12:59:55 +0100

    fixed duplicate join in util.php

--- php/util.php
+++ php/util.php
@@ -10,11 +10,7 @@ const JOINS=
 " INNER JOIN motorcycle_involved_def ON motorcycle_involved_def.motorcycle_involved = accident_data.motorcycle_involved" .
 " INNER JOIN delivery_van_involved_def ON delivery_van_involved_def.delivery_van_involved = accident_data.delivery_van_involved" .
 " INNER JOIN truck_bus_or_tram_involved_def ON truck_bus_or_tram_involved_def.truck_bus_or_tram_involved = accident_data.truck_bus_or_tram_involved" .
-" INNER JOIN road_surface_condition_def ON road_surface_condition_def.road_surface_condition = accident_data.road_surface_condition" .
-" INNER JOIN land_def ON land_def.land = accident_data.land" .
-" INNER JOIN kind_def ON kind_def.kind = accident_data.kind" .
-" INNER JOIN category_def ON category_def.category = accident_data.category" .
-" INNER JOIN type_def ON type_def.type = accident_data.type";
+" INNER JOIN road_surface_condition_def ON road_surface_condition_def.road_surface_condition = accident_data.road_surface_condition";
 
 function send_json_err($msg){
     echo "{\"error\":\"$msg\"}";
