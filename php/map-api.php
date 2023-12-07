8f7ac3824e895f34a35b6db614da618220f69d1d
93094049+kraemr@users.noreply.github.com <93094049+kraemr@users.noreply.github.com>
2023-12-07 23:34:40 +0100

    Can now properly filter for region,year and outcome in map-view

--- php/map-api.php
+++ php/map-api.php
@@ -12,15 +12,38 @@ try {
         echo '{"error":"year is missing"}';
         return;
     }
+    $land = 0;
+    if(isset($_GET["land"])){
+        $land = $_GET["land"];
+    }
+
+    $accident_type = -1;
+    if(isset($_GET["kind"])){
+        $accident_type = $_GET["kind"];
+    }
+
     $json_obj = array();
-    
     require "db-conn.php";
     $conn = connect_db();
     // Process other options here
-    
-    $query = $conn->prepare("Select ID,longitude,latitude from accident_data where year = ?");
-    $query->bindValue(1,$year);
-    $query->execute();
+    if($land > 0 && $accident_type == -1){ // filter by year and land
+        $query = $conn->prepare("Select ID,longitude,latitude from accident_data where year = ? and land = ?");
+        $query->bindValue(1,$year);
+        $query->bindValue(2,$land);
+        $query->execute();
+    }
+    else if($land > 0 && $accident_type != -1){ // filter by year land and type
+        $query = $conn->prepare("Select ID,longitude,latitude from accident_data where year = ? and land = ? and kind = ?");
+        $query->bindValue(1,$year);
+        $query->bindValue(2,$land);
+        $query->bindValue(3,$accident_type);
+        $query->execute();
+    }
+    else{ // filter only by year , if all get params except year are empty or = 0
+        $query = $conn->prepare("Select ID,longitude,latitude from accident_data where year = ?");
+        $query->bindValue(1,$year);
+        $query->execute();
+    }
     $i = 0;
     
     while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
@@ -32,7 +55,6 @@ try {
 }
 catch(PDOException $e) {
     header('Content-Type: application/json');
-    echo "{'error':'database error'}";
+    echo " 'error':'database error' ";
 }
-
 ?>
