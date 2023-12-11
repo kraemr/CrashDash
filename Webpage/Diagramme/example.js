7bbdd95211b44d98222451beee965365f6c532dc
93094049+kraemr@users.noreply.github.com <93094049+kraemr@users.noreply.github.com>
2023-12-11 14:25:07 +0100

    HELP ME IM GOING INSANE

--- Webpage/Diagramme/example.js
+++ Webpage/Diagramme/example.js
@@ -3,11 +3,13 @@ var url = "../php/get-stats.php";
 xmlhttp.open("POST", url);
 xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 xmlhttp.send(JSON.stringify({
-    "condition_column":"year",
-    "condition_value":2022,
-    "operator":"=",
-    "group_by":"land",
-    "order_by":"land",
-    "asc":false,
-    "columns":["year","land"]
+    "columns":["kind"],"group_by":"kind"
+}));
+
+var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+var url = "../php/get-stats.php";
+xmlhttp.open("POST", url);
+xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+xmlhttp.send(JSON.stringify({
+    "columns":["day"],"group_by":"day"
 }));
