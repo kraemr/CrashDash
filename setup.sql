31228cf48e2a53fcac0341618ffb539e1b92df34
93094049+kraemr@users.noreply.github.com <93094049+kraemr@users.noreply.github.com>
2023-12-13 16:56:57 +0100

    fixed long load times with indexes

--- setup.sql
+++ setup.sql
@@ -29,12 +29,6 @@ Create Table accident_data(
     latitude FLOAT, # y
     CONSTRAINT PK_Accident_Data PRIMARY KEY (Id)
 );
-CREATE INDEX IX_accident_year ON accident_data(year);
-CREATE INDEX IX_accident_month ON accident_data(month);
-CREATE INDEX IX_accident_region ON accident_data(region);
-CREATE INDEX IX_accident_land ON accident_data(land);
-CREATE INDEX IX_accident_kind ON accident_data(kind);
-CREATE INDEX IX_accident_category ON accident_data(category);
 
 Create Table land_def(
     land SMALLINT,
@@ -125,7 +119,27 @@ CREATE TABLE day_def (
     day_str TINYTEXT,
     CONSTRAINT PK_day_def PRIMARY KEY (day)
 );
+CREATE INDEX IX_accident_year ON accident_data(year);
+CREATE INDEX IX_accident_month ON accident_data(month);
+CREATE INDEX IX_accident_region ON accident_data(region);
+CREATE INDEX IX_accident_land ON accident_data(land);
+CREATE INDEX IX_accident_kind ON accident_data(kind);
+CREATE INDEX IX_accident_category ON accident_data(category);
+
 
 CREATE INDEX IX_land_def ON land_def(land);
+CREATE INDEX IX_munincipality_def ON munincipality_def(munincipality);
 CREATE INDEX IX_kind_def ON kind_def(kind);
-CREATE INDEX IX_cat_def  ON category_def(category);  
+CREATE INDEX IX_category_def ON category_def(category);
+CREATE INDEX IX_type_def ON type_def(type);
+CREATE INDEX IX_district_def ON district_def(district);
+CREATE INDEX IX_light_condition_def ON light_condition_def(light_condition);
+CREATE INDEX IX_bycicle_involved_def ON bycicle_involved_def(bycicle_involved);
+CREATE INDEX IX_car_involved_def ON car_involved_def(car_involved);
+CREATE INDEX IX_passenger_involved_def ON passenger_involved_def(passenger_involved);
+CREATE INDEX IX_motorcycle_involved_def ON motorcycle_involved_def(motorcycle_involved);
+CREATE INDEX IX_delivery_van_involved_def ON delivery_van_involved_def(delivery_van_involved);
+CREATE INDEX IX_truck_bus_or_tram_involved_def ON truck_bus_or_tram_involved_def(truck_bus_or_tram_involved);
+CREATE INDEX IX_road_surface_condition_def ON road_surface_condition_def(road_surface_condition);
+CREATE INDEX IX_day_def ON day_def(day);
+
